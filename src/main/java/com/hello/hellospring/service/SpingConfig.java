01d1409d1a48377bb9ded75af881cfa5cb6e3cf7
YOUNGBINJEON <sh44972@gmail.com>
2021-05-18 02:39:44 +0900

    [Add] 순수 JDBC 구현연동

--- src/main/java/com/hello/hellospring/service/SpingConfig.java
+++ src/main/java/com/hello/hellospring/service/SpingConfig.java
@@ -1,12 +1,22 @@
 package com.hello.hellospring.service;
 
+import com.hello.hellospring.repository.JdbcMemberRepository;
 import com.hello.hellospring.repository.MemberRepository;
-import com.hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpingConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpingConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -15,6 +25,8 @@ public class SpingConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
